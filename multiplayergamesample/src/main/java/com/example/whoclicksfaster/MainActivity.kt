31d50ec96a84ed06af5008b592ec5afb121b5a28
wangzixuan <wangzixuan@youzan.com>
2018-10-11 21:27:46 +0800

    feature: implement create private group and join private group

--- multiplayergamesample/src/main/java/com/example/whoclicksfaster/MainActivity.kt
+++ multiplayergamesample/src/main/java/com/example/whoclicksfaster/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.whoclicksfaster
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import com.example.payment.KeyStoreHelper
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
-import network.celer.mobile.Client
-import network.celer.mobile.Mobile
+import network.celer.mobile.*
 import java.io.File
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GroupCallback {
+
 
     private var keyStoreString = ""
     private var passwordStr = ""
@@ -23,6 +24,8 @@ class MainActivity : AppCompatActivity() {
     private var transferAmount: String = "30000000000000000" // 0.03 ETH
 
     private var client: Client? = null
+    private lateinit var gc: GroupClient
+    lateinit var joinAddr: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         passwordStr = KeyStoreHelper().getPassword()
 
         var keyStoreJson = Gson().fromJson(keyStoreString, KeyStoreData::class.java)
-        var joinAddr = "0x" + keyStoreJson.address
+        joinAddr = "0x" + keyStoreJson.address
 
         addLog("keyStoreString: ${keyStoreString}")
         Log.d("MainActivity", keyStoreString)
@@ -65,23 +68,9 @@ class MainActivity : AppCompatActivity() {
 
                 }
 
-                // check if an address has joined Celer Network
-                try {
-                    receiverAddr = "0x2718aaa01fc6fa27dd4d6d06cc569c4a0f34d399"
-                    val hasJoin = client?.hasJoinedCeler(receiverAddr)
-                    addLog("hasJoin: $hasJoin")
-                } catch (e: Exception) {
-                    addLog("check Error: ${e.localizedMessage}")
-                }
+                onNewGroupClient()
 
-                // send cETH to an address
-                try {
-                    client?.sendPay(receiverAddr, transferAmount)
-                    addLog("sendPay: done")
-                } catch (e: Exception) {
-                    addLog("send cETH Error: ${e.localizedMessage}")
 
-                }
 
             }
 
@@ -94,9 +83,52 @@ class MainActivity : AppCompatActivity() {
         })
 
 
-
-
     }
+
+
+    fun onNewGroupClient() {
+        try {
+            gc = Mobile.newGroupClient("group-prod-ropsten.celer.app:10001", keyStoreString, passwordStr, this)
+            addLog("Connected to Group Server")
+        } catch (e: Exception) {
+            addLog("NewGroupClient failed: " + e.toString())
+            Log.e("NewGroupClient failed: ", e.toString())
+        }
+    }
+
+    override fun onRecvGroup(gresp: GroupResp?, err: String?) {
+        addLog("OnRecvGroup:")
+        addLog(gresp.toString())
+        addLog(err)
+    }
+
+    fun onCreatePrivate(v: View) {
+        var g = Group()
+        g.myId = keyStoreString
+        g.code = 111111
+        g.stake = "1000000000000000000"
+        addLog("Bot: " + g.toString())
+        try {
+            gc.createPrivate(g)
+        } catch (e: Exception) {
+            addLog(e.toString())
+        }
+    }
+
+    fun onJoinPrivate(v: View) {
+        var g = Group()
+        g.myId = keyStoreString
+        g.code = 111111
+        g.stake = "10"
+        addLog("Join: " + g.toString())
+
+        try {
+            gc.joinPrivate(g)
+        } catch (e: Exception) {
+            addLog(e.toString())
+        }
+    }
+
 
     private fun generateFilePath() {
         val generaFile = File(this.filesDir.path, "celer")
